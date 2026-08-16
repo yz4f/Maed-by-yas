0dc262e7131f7936d179ac309bc30f1909cb34b7
yz4f <yasemoh24@gmail.com>
2026-08-17 01:03:21 +0300

    Remove hardcoded NEXTAUTH_URL from auth route
    Removed hardcoded NEXTAUTH_URL from route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-process.env.NEXTAUTH_URL = 'https://t3n-store-production.up.railway.app';
-
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
